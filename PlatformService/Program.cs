1c6e4cc6421377b9ce88d1622c581ef53543b3bb
nidarprog <ahmedismael12@gmail.com>
2023-12-15 22:20:35 +0100

    Add swagger doc

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -9,7 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("V1", new Microsoft.OpenApi.Models.OpenApiInfo 
+    {
+        Title = "PlatformService",
+        Version ="v1"
+    });
+}
+);
 builder.Services.AddDbContext<AppDbContext>(opt =>
                 opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
